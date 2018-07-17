a3a5e7210a42e76b08bfc03848474390ac4e079e
IndreRod <i.rodiniene@gmail.com>
2018-07-17 17:43:24 +0300

    Nd

--- JAVA/staciakampis.js
+++ JAVA/staciakampis.js
@@ -6,3 +6,25 @@ var plotas=(splotis*saukstis);
 var istrizaine=500;
 
 document.getElementById('test').innerHTML = istrizaine;
+
+
+
+
+
+
+
+if (4 <5 ) {console.log ('4 yra daugiau uz 5');}
+else {
+    sssssssss;
+}
+
+
+var gim_metai=1988;
+var dabar_m=2018;
+
+if (dabar_m > gim_metai) {
+    console.log ("Taip");
+}
+else {
+    console.log ("Ne");
+}
