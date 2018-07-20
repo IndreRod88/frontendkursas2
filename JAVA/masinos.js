a69ff9016ecb4b7c14ca9bd2abd8351045bdb5bd
IndreRod <i.rodiniene@gmail.com>
2018-07-20 23:47:17 +0300

    nd

--- JAVA/masinos.js
+++ JAVA/masinos.js
@@ -1,4 +1,4 @@
-var autos = [
+let autos = [
     ['2017-07-06 19:59:41','ABC123',5000,200],
     ['2017-07-06 19:59:45','ABC456',1564,50],
     ['2017-07-06 19:59:45','ABC789',8432,123],
@@ -11,14 +11,14 @@ var autos = [
     null, undefined,
 ];
 
-var new_autos;
-var a;
-var b;
-var i=0;
+let new_autos;
+let a;
+let b;
+let i=0;
 myarr=[];
 autos.forEach(function(auto) {
     if (auto !== null && auto !== undefined) {
-        var string = '<tr>';
+        let string = '<tr>';
         auto.forEach(function(item) {
             string += '<td>' + item +'</td>';
         });
@@ -28,31 +28,43 @@ autos.forEach(function(auto) {
         ats=(a/1000) / (b/3600);
         a=0;
         b=0;
-        atsak=Math.round(ats);
-        string += '<td>'+ atsak +'</td>';
+        string += '<td>'+ Math.round(ats) +'</td>';
         string += '</tr>';
         document.getElementById('duomenys').innerHTML += string;
     }
 });
 
 function addTo() {
-    let data1 = document.getElementById("data1").value;
+    let data_laikas = document.getElementById("data1").value;
     let numeris = document.getElementById("data2").value;
     let greitis = document.getElementById("data3").value;
     let atstumas = document.getElementById("data4").value;
     let table = document.getElementById("myTable");
+
+    if ((data_laikas!=="") && (numeris!=="") && (greitis!=="") && (atstumas!=="")) {
+
+    autos.push(data_laikas);
     let row = table.insertRow(-1);
     let cell1 = row.insertCell(0);
+    let data_laikas_last=autos.length-1;
+    cell1.innerHTML = autos[data_laikas_last];
+
+    autos.push(numeris);
     let cell2 = row.insertCell(1);
+    let numeris_last=autos.length-1;
+    cell2.innerHTML = autos[numeris_last];
+
+    autos.push(greitis);
     let cell3 = row.insertCell(2);
+    let greitis_last=autos.length-1;
+    cell3.innerHTML = autos[greitis_last];
+
     let cell4 = row.insertCell(3);
-    let cell5 = row.insertCell(4);
-    cell1.innerHTML = data1;
-    cell2.innerHTML = numeris;
-    cell3.innerHTML = greitis;
     cell4.innerHTML = atstumas;
+    let cell5 = row.insertCell(4);
     let kmh=(greitis/1000) / (atstumas/3600);
-    cell5.innerHTML = kmh;
+    cell5.innerHTML = Math.round(kmh);
+}
 }
 
 
